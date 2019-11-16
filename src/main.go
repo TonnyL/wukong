785e1c55e9b8ffdf19f4ae75bd3f5a5ed240d2ee
tonnyl <lizhaotailang@gmail.com>
2019-11-16 20:43:35 +0800

    add functions of querying repos/devs/langs

--- src/main.go
+++ src/main.go
@@ -1,5 +1,72 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
 func main() {
-	
+
+}
+
+// Receive an array of trending repositories.
+func FetchTrendingRepositories(language, since string) ([]Repository, error) {
+	resp, err := http.Get(fmt.Sprintf("https://github-trending-api.now.sh/repositories?language=%s&since=%s", language, since))
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	repositories := make([]Repository, 0)
+	jsonErr := json.NewDecoder(resp.Body).Decode(&repositories)
+	if jsonErr != nil {
+		return nil, jsonErr
+	}
+
+	return repositories, nil
+}
+
+// Receive an array of trending developers.
+func FetchTrendingDevelopers(language, since string) ([]Developer, error) {
+	resp, err := http.Get(fmt.Sprintf("https://github-trending-api.now.sh/developers?language=%s&since=%s", language, since))
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	developers := make([]Developer, 0)
+	jsonErr := json.NewDecoder(resp.Body).Decode(&developers)
+	if jsonErr != nil {
+		return nil, jsonErr
+	}
+
+	return developers, nil
+}
+
+// Read popular languages and all languages.
+func ReadLanguages() ([]Language, error) {
+	jsonFile, err := os.Open("./resources/languages.json")
+	if err != nil {
+		return nil, err
+	}
+
+	defer jsonFile.Close()
+
+	bytes, err := ioutil.ReadAll(jsonFile)
+	if err != nil {
+		return nil, err
+	}
+
+	languages := make([]Language, 0)
+	jsonErr := json.Unmarshal(bytes, &languages)
+	if jsonErr != nil {
+		return nil, jsonErr
+	}
+
+	return languages, nil
 }
