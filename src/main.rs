7f1a6c67d76afb1a2f621460c4cb526c5893671e
tonnyl <lizhaotailang@gmail.com>
2020-05-02 14:18:22 +0800

    add data struct definition

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct Repository {
+    author: String,
+    name: String,
+    description: String,
+    language: Option<String>,
+    stars: i32,
+    #[serde(rename = "currentPeriodStars")]
+    current_period_stars: i32,
+}
+
+#[derive(Deserialize)]
+struct Developer {
+    username: String,
+    name: String,
+    repo: Repo,
+}
+
+#[derive(Deserialize)]
+struct Repo {
+    name: String,
+    description: String,
+}
+
+#[derive(Deserialize)]
+struct Language {
+    #[serde(rename = "urlParam")]
+    url_param: String,
+    name: String,
+}
+
 fn main() {
     println!("Hello, world!");
 }
